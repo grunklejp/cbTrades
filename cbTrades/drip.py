987342b018d729373a16c125e68c998fe29b3a94
John Paul Kiser <johnpaulkiser@gmail.com>
2019-01-08 18:52:36 -0500

    Adds drip.py and check_order_status method to selector.py

---
+++ cbTrades/drip.py
@@ -0,0 +1,40 @@
+from selector import Selector
+import time
+
+def main():
+    s = Selector(0.05, 0.05, 'ETH-USD', 0.01)
+    open_sells = []
+    open_sells.insert(0, s.sell())
+    open_buy = s.buy()
+    trading = True
+    
+    while trading:
+        try:
+            while True:
+                time.sleep(0.5)
+                status = s.check_order_status(open_sells[-1], open_buy) #returns 'bought' or 'sold'
+                
+                if status == 'bought':
+                    open_sells.insert(0, s.sell())
+                    open_buy = s.buy()
+
+                if status == 'sold':
+                    open_sells.pop()
+                    s.cancel(open_buy)
+                    open_buy = s.buy()
+                    open_sells.insert(0, s.sell())
+                print(status)
+        except KeyboardInterrupt:
+            print('Closed')
+            break
+        
+        
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
--- cbTrades/selector.py
+++ cbTrades/selector.py
@@ -4,6 +4,7 @@ sets currency and buy/sell spreds
 and price of orders
 '''
 import cbpro
+import time
 from testclient import TestClient
 from auth import Account
 
@@ -31,22 +32,49 @@ class Selector:
     def buy(self): 
         price = self.get_price(self.pair)
         ord = self.client.place_limit_order(self.pair, 'buy', price-self.bdi, self.amount)
-        return ord['id']
-
+        try:
+            return ord['id']
+        except KeyError:
+            print('order failed trying again')
+            time.sleep(0.5)
+            self.buy()
+        
     def sell(self):
         price = self.get_price(self.pair)
         ord = self.client.place_limit_order(self.pair, 'sell', price+self.profit, self.amount)
-        return ord['id']
+        
+        try:
+            print(ord['id'])
+            return ord['id']
+        except KeyError:
+            print('order failed trying again')
+            time.sleep(0.5)
+            self.sell()
+    
+    def cancel(self, id):
+        self.client.cancel_order(id)
 
     def get_price(self, pair):
         tick = self.client.get_product_ticker(pair)
         return float(tick['price'])
+    
+    def check_order_status(self, sell_id, buy_id):
+        s = self.client.get_order(sell_id)
+        b = self.client.get_order(buy_id)
+        if s['status'] == 'done':
+            return 'sold'
+        if b['status'] == 'done':
+            return 'buy'
+        return 'none'
+
+         
 
     
 if __name__ == "__main__":
     s = Selector(0.05, 0.05, 'BTC-USD', 0.005)
     s.get_price('BTC-USD')
-    s.sell()
+    id = s.sell()
+    
 
 
 
